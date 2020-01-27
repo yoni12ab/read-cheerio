16117f5fb21dbb3717e909c112fcc4fbc1728152
Yoni Argovi <yoni12ab@gmail.com>
2020-01-27 20:24:01 +0200

    first commit

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const $ = cheerio.load(fs.readFileSync('example.html'));
 
 const ATTR = 'data-mgmtAutomationId';
 
-const allIds = $(`[${ATTR}]`).map((i, element) =>
-  $(element).attr(ATTR.toLowerCase())
-);
+const allIds = $(`[${ATTR}]`)
+  .map((i, element) => $(element).attr(ATTR.toLowerCase()))
+  .toArray();
 
 console.log(allIds);
