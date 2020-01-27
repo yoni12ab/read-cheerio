a0942843317287acf5d612d942f6923da1268fb4
Yoni Argovi <yoni12ab@gmail.com>
2020-01-27 20:21:18 +0200

    'first'

---
+++ index.js
@@ -0,0 +1,11 @@
+const cheerio = require('cheerio');
+var fs = require('fs');
+const $ = cheerio.load(fs.readFileSync('example.html'));
+
+const ATTR = 'data-mgmtAutomationId';
+
+const allIds = $(`[${ATTR}]`).map((i, element) =>
+  $(element).attr(ATTR.toLowerCase())
+);
+
+console.log(allIds);
